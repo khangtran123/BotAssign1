54c78d833bda9e15d40583a85be401ab5f32d4cf
markbatin <markbatin@gmail.com>
2016-04-09 12:59:36 -0700

    fixed some errors in assemble page
    Signed-off-by:markbatin <markbatin@gmail.com>
    Signed-off-by:markbatin <markbatin@gmail.com>

--- application/views/assemblePage.php
+++ application/views/assemblePage.php
@@ -3,24 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div id="title">
-<h1>Bot Assembly</h1>
+    <h1>Bot Assembly</h1>
 </div>
 <div>
- {playerCards}
+    {playerCards}
 </div>
 <div id="assemblyRightContainer">
-<table>
-    <tr><td>Head<select class="buttonAssemble" >{selectHeads}</select></td></tr>
-    <tr><td><br/><br/></td></tr>
-    <tr><td>Body<select class="buttonAssemble" >{selectBody}</select></td></tr>
-    <tr><td><br/><br/></td></tr>
-    <tr><td>Legs<select class="buttonAssemble" >{selectLegs}</select></td></tr>
-                <tr><td><br/><br/></td></tr>
-    <tr><td><input class="buttonAssemble" type="submit" name='btn_submit' value='Assemble'/></td></tr>            
-    <tr><td><h5 id="previewH5"><br/>Preview</h5></td></tr>
-    <tr><td><img src="../../assets/images/11c-0.jpeg"</td></tr>
-    <tr><td><img src="../../assets/images/11c-1.jpeg"</td></tr>
-    <tr><td><img src="../../assets/images/11c-2.jpeg"</td></tr>
-</table>
+    <table>
+        <tr><td>Head<select class="buttonAssemble" >{selectHeads}</select></td></tr>
+        <tr><td><br/><br/></td></tr>
+        <tr><td>Body<select class="buttonAssemble" >{selectBody}</select></td></tr>
+        <tr><td><br/><br/></td></tr>
+        <tr><td>Legs<select class="buttonAssemble" >{selectLegs}</select></td></tr>
+        <tr><td><br/><br/></td></tr>
+        <tr><td><input class="buttonAssemble" type="submit" name='btn_submit' value='Assemble'/></td></tr>            
+        <tr><td><h5 id="previewH5"><br/>Preview</h5></td></tr>
+        <tr><td><img src="../../assets/images/11c-0.jpeg"</td></tr>
+        <tr><td><img src="../../assets/images/11c-1.jpeg"</td></tr>
+        <tr><td><img src="../../assets/images/11c-2.jpeg"</td></tr>
+    </table>
 </div>
-   
