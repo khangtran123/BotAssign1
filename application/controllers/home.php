54c2a720d8d7cd5e56f9177263cccd24b0ced54d
markbatin <markbatin@gmail.com>
2016-04-14 12:32:52 -0700

    fixed some errors in assemble page
    Signed-off-by:markbatin <markbatin@gmail.com>
    Signed-off-by:markbatin <markbatin@gmail.com>

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,31 +4,31 @@
  * application/controllers/Home.php
  */
 
-class home extends Application
-{
+class home extends Application {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
     }
 
-    public function index()
-    {
+    public function index() {
         $this->data['pageBody'] = 'homepage';
         $this->gameStatus();
         $this->playerInfo();
+        $this->gameRefresh();
+        $this->data['gameNumber'] = $this->getRound();
+        $this->data['gameState'] = $this->getState();
+        $this->data['countdown'] = $this->getCountdown();
+        $this->render();
     }
-	
-    private function gameStatus()
-    {
+
+    private function gameStatus() {
         $this->load->model('gameStatus');
         $query = $this->gameStatus->gameSummary();
 
         $gameStatus = array();
 
-        foreach ($query as $stat)
-        {
-            $gameSummary[] = (array) $stat;	
+        foreach ($query as $stat) {
+            $gameSummary[] = (array) $stat;
         }
 
         $Status['seriesInfo'] = $gameSummary;
@@ -36,24 +36,21 @@ class home extends Application
         $this->data['gameStatus'] = $this->parser->parse('_seriesInfo', $Status, true);
     }
 
-    private function playerInfo() 
-    {
+    private function playerInfo() {
         $this->load->model('playerInfo');
 
         //try to call the query in the model to initialize it
         $query = $this->playerInfo->playerEC();
         $playerInfo = array();
 
-        foreach ($query as $row)
-        {
+        foreach ($query as $row) {
             $playerInfo[] = (array) $row;
         }
-        
+
         $table = array();
-        foreach ($playerInfo as $index => $row)
-        {
+        foreach ($playerInfo as $index => $row) {
             $new = $row;
-            switch($index % 2 == 0){
+            switch ($index % 2 == 0) {
                 case TRUE:
                     $new['tableClass'] = "firstColumn";
                     break;
@@ -64,8 +61,50 @@ class home extends Application
             $table[] = $new;
         }
         $players['playerTable'] = $table;
-        $this->data['playerInfo'] = $this->parser->parse('_playerTable', $players, true);        
-        $this->render();
+        $this->data['playerInfo'] = $this->parser->parse('_playerTable', $players, true);
+    }
+
+    public function gameRefresh() {
+        $status = $this->curl->simple_get('http://botcards.jlparry.com/status');
+        $xml = @simplexml_load_string($status);
+        //creates a variable for sta
+        $this->round = (int) $xml->round;
+        $this->countdown = (int) $xml->countdown;
+        $this->state = (int) $xml->state;
+        $this->status = 1;
+    }
+
+    public function getRound() {
+        return $this->round;
+    }
+
+    public function getCountdown() {
+        return $this->countdown;
+    }
+
+    public function getState() {
+        switch ($this->state) {
+            case 0:
+                $state = "The game is currently not active";
+                break;
+            case 1:
+                $state = "The game is setting up.";
+                break;
+            case 2:
+                $state = "The game is ready and market is open.";
+                break;
+            case 3:
+                $state = "The game is active.";
+                break;
+            case 4:
+                $state = "The game is over.";
+                break;
+            default:
+                $state = "The game is unavailable";
+                break;
+        }
+
+        return $state;
     }
 
 }
